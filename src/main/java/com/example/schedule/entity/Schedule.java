28c2d95015491352c34daed64ab212555b4a3197
anyubinn <anyubin0724@naver.com>
2025-03-28 14:54:16 +0900

    feat: Lv1. 일정 entity 추가

--- src/main/java/com/example/schedule/entity/Schedule.java
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -1,2 +1,28 @@
-package com.example.schedule.entity;public class Schedule {
+package com.example.schedule.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "schedule")
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "longtext")
+    private String contents;
 }
