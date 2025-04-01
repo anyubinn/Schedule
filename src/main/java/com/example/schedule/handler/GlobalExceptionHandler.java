5e9c124335619e52b300247bade2e97102523d85
anyubinn <anyubin0724@naver.com>
2025-04-01 19:11:16 +0900

    feat: ResponseStatusException 예외 응답 메시지 추가
    - 에러 메시지가 응답에 보이도록 추가

--- src/main/java/com/example/schedule/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/schedule/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.schedule.handler;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,5 +25,16 @@ public class GlobalExceptionHandler {
         }
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException exception) {
+
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", exception.getStatusCode().value());
+        errors.put("error", exception.getReason());
+
+        return ResponseEntity.status(exception.getStatusCode()).body(errors);
     }
 }
